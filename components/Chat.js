01d670dece9baad8a359fe48ee8ecd2d143bd525
sms165 <61697688+sms165@users.noreply.github.com>
2022-07-30 19:00:08 +0200

    Revert "Update Chat.js"
    This reverts commit 8a10e250d9f3e522f40cf78c102d80a2874882ae.

--- components/Chat.js
+++ components/Chat.js
@@ -128,7 +128,7 @@ export default class Chat extends React.Component {
   componentWillUnmount() {
    
       
-      this.authUnsubscribe();
+      // this.authUnsubscribe();
     }
   
 
@@ -150,8 +150,7 @@ export default class Chat extends React.Component {
   }
 
   // Add message to Firestore
-  addMessages = (messages) => {console.log(messages);
-    const message = messages[0]
+  addMessages = (message) => {console.log(message);
     addDoc(messagesRef, {
       
       uid: this.state.uid,
@@ -192,8 +191,7 @@ export default class Chat extends React.Component {
       bgColor = '#8A95A5';
     }
 
-    let name = this.props.route.params.name;
-   // OR ...
+    let name = this.props.route.params.name; // OR ...
 //     // let { name } = this.props.route.params;
 //     let bgColor = this.props.route.params.bgColor;
 
@@ -207,15 +205,13 @@ export default class Chat extends React.Component {
                   flex: 1,
                 }}
               >
-                
         
                 <GiftedChat
                 renderBubble={this.renderBubble.bind(this)}
                   messages={this.state.messages}
                   onSend={(messages) => this.addMessages(messages)}
                   user={{
-                    _id: this.state.uid,
-                    name: this.state.user.name
+                    _id: 1,
                   }}
                 />
         
@@ -231,4 +227,177 @@ export default class Chat extends React.Component {
         }
         
         
-    
+        
+        
+
+//    //Firestore Database
+//    const firebase = require('firebase');
+//    require('firebase/firestore');
+   
+//    //// firebase adding credential in order to connect to firebase
+//    const firebaseConfig = {
+//        apiKey: "AIzaSyDhvK9oHmouNpSo8yaXKK_44cjkwyaE-dU",
+//        authDomain: "chat-app-e1449.firebaseapp.com",
+//        projectId: "chat-app-e1449",
+//        storageBucket: "chat-app-e1449.appspot.com",
+//        messagingSenderId: "555781147632",
+//        appId: "1:555781147632:web:e01436956f6c218625eb35",
+      
+//      };
+
+// export default class Chat extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       messages: [],
+//     };
+
+
+  
+ 
+//    if (!firebase.apps.length){
+//      firebase.initializeApp(firebaseConfig);
+//      }
+//      this.referenceChatMessages = firebase.firestore().collection("messages");
+
+//      this.referenceMessagesUser= null;
+//   }
+
+ 
+  
+
+ 
+
+//   componentDidMount() {
+
+//     this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+//         if (!user) {
+//           firebase.auth().signInAnonymously();
+//         }
+//         this.setState({
+//           uid: user.uid,
+//           messages: [],
+//           user: {
+//             _id: user.uid,
+//             name: name,
+//             avatar: "https://placeimg.com/140/140/any",
+//         },
+//         });
+//         this.unsubscribe = this.referenceChatMessages
+//           .orderBy("createdAt", "desc")
+//           .onSnapshot(this.onCollectionUpdate);
+//       });
+//     }
+   
+// //     this.referenceChatMessages = firebase.firestore().collection('messages');
+// //   this.unsubscribe = this.referenceChatMessages.onSnapshot(this.onCollectionUpdate)
+//    // this.setState({
+//     //   messages: [
+//     //     {
+//     //       _id: 1,
+//     //       text: "Helllo developer",
+//     //       createdAt: new Date(),
+//     //       user: {
+//     //         _id: 2,
+//     //         name: "React ative",
+//     //         avatar: "http://placeimg.com/140/140/any",
+//     //       },
+//     //     },
+//     //     {
+//     //       _id: 2,
+//     //       text: 'This is a system message',
+//     //       createdAt: new Date(),
+//     //       system: true,
+//     //      },
+//     //   ],
+//     // });
+
+
+//  componentWillUnmount() {
+//    this. unsubscribe();
+// }
+  
+
+//   onCollectionUpdate = (querySnapshot) => {
+//    const messages = [];
+//    // go through each document
+//    querySnapshot.forEach((doc) => {
+//      // get the QueryDocumentSnapshot's data
+//      let data = doc.data();
+//      messages.push({
+//        _id: data._id,
+//        text: data.text,
+//        createdAt: data.createdAt.toDate(),
+//        user: data.user,
+//      });
+//    });
+//   }
+
+//    // Add message to Firestore
+//    addMessages = (message) => {
+//     this.referenceChatMessages.add({
+//         uid: this.state.uid,
+//         _id: message._id,
+//       text: message.text || '',
+//       createdAt: message.createdAt,
+//       user: message.user,
+      
+//     });
+//   };
+  
+//   onSend(messages = []) {
+//     this.setState((previousState) => ({
+//       messages: GiftedChat.append(previousState.messages, messages),
+//     }));
+//   }
+
+//   renderBubble(props) {
+//     return (
+//       <Bubble
+//         {...props}
+//         wrapperStyle={{
+//           right: {
+//             backgroundColor: '#000'
+//           }
+//         }}
+//       />
+//     )
+//   }
+
+//   render() {
+//     let name = this.props.route.params.name; // OR ...
+//     // let { name } = this.props.route.params;
+//     let bgColor = this.props.route.params.bgColor;
+
+//     this.props.navigation.setOptions({ title: name });
+
+//     return (
+//       <View
+//         style={{
+//           backgroundColor: bgColor,
+//           flex: 1,
+//         }}
+//       >
+
+//         <GiftedChat
+//         renderBubble={this.renderBubble.bind(this)}
+//           messages={this.state.messages}
+//           onSend={(messages) => this.onSend(messages)}
+//           user={{
+//             _id: 1,
+//           }}
+//         />
+
+
+        
+//           {/*fix error on display of keyboard for android devices  */}
+//         {Platform.OS === "android" ? (
+//           <KeyboardAvoidingView behavior="height" />
+//         ) : null}
+//       </View>
+//     );
+//   }
+// }
+
+
+
